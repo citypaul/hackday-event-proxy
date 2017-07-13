9098a147fc5f544f8ebcc0b19f7b2694f5578978
Paul Hammond <paul.hammond@bbc.co.uk>
2017-07-13 15:55:27 +0100

    remove console.log

--- src/api/events/post-event.js
+++ src/api/events/post-event.js
@@ -1,11 +1,10 @@
-import Boom from 'boom';
+// import Boom from 'boom';
 
 export default {
   method: 'POST',
   path: '/api/events',
   config: {
     handler: (request, reply) => {
-      console.log(request.payload);
       reply(request.payload);
     }
   }
